1ab021fd2ad3037eaffeae4bf7f8c9bfbe59116a
mmmghx <44229348+mmmghx@users.noreply.github.com>
2018-10-24 09:01:43 +0900

    til JS 34
    til JS 34

---
+++ colors.js
@@ -0,0 +1,45 @@
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'UA-127727509-1');
+
+var Body = {
+setColor :  function (color){
+        document.querySelector('body').style.color=color;
+    },
+setBackgroundColor : function (color){
+        document.querySelector('body').style.backgroundColor=color;
+    }
+}
+
+var Links = {
+setColor :  function (color){
+      var alist = document.querySelectorAll('a');
+      var i = 0;
+      while(i < alist.length){
+      console.log(alist[i]);
+      alist[i].style.color = color;
+      i++;
+      }
+    }
+}
+
+function nightDayHandler(self){
+var target = document.querySelector('body');
+if(self.value === 'night'){
+  Body.setBackgroundColor('black');
+  Body.setColor('white');
+  self.value = 'day';
+  Links.setColor('powderblue');
+}
+else {
+  Body.setBackgroundColor('white');
+  Body.setColor('black');
+  self.value = 'night';
+  Links.setColor('blue');
+}
+}
